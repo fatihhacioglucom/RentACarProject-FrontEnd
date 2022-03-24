2bf92e885528f3184513d254869b146e94d79b54
Fatih Hacıoğlu <fatihhacioglucom@gmail.com>
2022-03-24 22:27:53 +0300

    ListResponseModel added

--- src/app/services/car-detail.service.ts
+++ src/app/services/car-detail.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { CarDetailResponseModel } from '../models/carDetailResponseModel';
+import { CarDetail } from '../models/carDetail';
+import { ListResponseModel } from '../models/listResponseModel';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +12,7 @@ export class CarDetailService {
 
   constructor(private httpClient: HttpClient) {}
 
-  getCarDetails(): Observable<CarDetailResponseModel> {
-    return this.httpClient.get<CarDetailResponseModel>(this.apiUrl);
+  getCarDetails(): Observable<ListResponseModel<CarDetail>> {
+    return this.httpClient.get<ListResponseModel<CarDetail>>(this.apiUrl);
   }
 }
